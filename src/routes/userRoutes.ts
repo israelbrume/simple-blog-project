9cdd6d7b5867276c49686fb559e5f1024a443d3a
personal <oghenekaroisrael089@gmail.com>
2023-08-18 16:57:11 +0100

    added gitignore

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,8 +3,69 @@ const express3 = require('express');
 const userRouter = express3.Router();
 const { register, login} = require('../controllers/UserController');
 
+/**
+ * @swagger
+ * /users/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             example:
+ *               email: user@example.com
+ *               password: securePassword
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       400:
+ *         description: User registration failed
+ */
 userRouter.post('/register', register);
 
+/**
+ * @swagger
+ * /users/login:
+ *   post:
+ *     summary: Login a user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             example:
+ *               email: user@example.com
+ *               password: securePassword
+ *     responses:
+ *       200:
+ *         description: User logged in successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 user:
+ *                   $ref: '#/components/schemas/User'
+ *                 token:
+ *                   type: string
+ *       401:
+ *         description: Login failed
+ */
 userRouter.post('/login', login);
 
 module.exports = userRouter;
